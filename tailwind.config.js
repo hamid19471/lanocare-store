7dd8889ccedfcf7939e57dac493873b864e053d3
hamid19471 <hamid.19471@gmail.com>
2023-05-23 23:41:57 +0330

    Update: add costom theme attributes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#C99A57",
+        "secondary-color": "#000000",
+      },
+      fontSize: {
+        header: "3rem",
+        subheader: "1.3rem",
+        contnet: "1rem",
+      },
+      fontFamily: {
+        main: ["Oswald", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
