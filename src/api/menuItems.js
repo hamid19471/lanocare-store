6aa470f66e3f49f7db910e55a14d21caaecac242
hamid19471 <hamid.19471@gmail.com>
2023-05-23 23:33:37 +0330

    Create: menu items for API

---
+++ src/api/menuItems.js
@@ -0,0 +1,27 @@
+export const menuItems = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    path: "/about",
+  },
+  {
+    id: 3,
+    name: "Shop",
+    path: "/shop",
+  },
+  {
+    id: 4,
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact",
+  },
+];
