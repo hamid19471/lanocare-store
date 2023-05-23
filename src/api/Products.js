2619fc266db19d512fcec72468089d46f6a570e5
hamid19471 <hamid.19471@gmail.com>
2023-05-23 23:23:41 +0330

    Create: fakse product for API in application

---
+++ src/api/Products.js
@@ -0,0 +1,92 @@
+import wrinkle_featured from "../assets/product_images/deep anti weinkle/featured.png";
+import wrinkle_main from "../assets/product_images/deep anti weinkle/main-image.png";
+import wrinkle_box from "../assets/product_images/deep anti weinkle/box-image.png";
+
+import body_featured from "../assets/product_images/body lotion/featured.png";
+import body_main from "../assets/product_images/body lotion/main-image.png";
+import body_box from "../assets/product_images/body lotion/box-image.png";
+
+import skin_featured from "../assets/product_images/skin care/featured.png";
+import skin_main from "../assets/product_images/skin care/main-image.png";
+import skin_box from "../assets/product_images/skin care/box-image.png";
+
+import vitaminc_featured from "../assets/product_images/vitamin c/featured.png";
+import vitmainc_main from "../assets/product_images/vitamin c/main-image.png";
+import vitaminc_box from "../assets/product_images/vitamin c/box-image.png";
+
+import vitamine_featured from "../assets/product_images/vitamin e/featured.png";
+import vitamine_main from "../assets/product_images/vitamin e/main-image.png";
+import vitamine_box from "../assets/product_images/vitamin e/box-image.png";
+
+export const products = [
+  {
+    id: 1,
+    name: "Anti-Wrinkle Cream",
+    description:
+      "Cell formation, skin regeneration, the deep penetration of the effective ingredients of the cream into your skin without the harmful rays of the sun are of the reasons for the need to use anti-wrinkle cream. LANOCARE anti-wrinkle cream is specially produced with modern science and technology and has a powerful anti-wrinkle effect. In addition to its effective preventive effect, this product also plays a significant role in the process of reducing tiny and deep skin wrinkles and makes the skin smooth and soft with a function similar to Botox. Increasing the thickness and firmness of the skin, clarifying the skin and increasing the softness are other benefits of this product. Another important point to slow down the aging process of the skin is to provide moisture and protect the skin.",
+    short_description:
+      "LANOCARE anti-wrinkle cream is a strong moisturizer which hydrates the skin and nourishes and soothes tired skin. Among the actives used in the formula of this product are the following: Peptide syn-ake This peptide, produced by the PENTA PHARMA Swiss Company, is a new and efficient compound with the same effect as Botox, which, in addition to inhibiting muscle contraction, makes wrinkles smooth. Pepha-tight CB This substance has an immediate lifting effect, and also brings a permanent and stable effect after continuous use. Smoother skin, reduction of wrinkles and uniform skin texture are also other benefits of this substance. Usable For: Facial and neck skin Suitable for normal to dry and very dry skin",
+    price: 399,
+    feature_image: wrinkle_featured,
+    product_image: wrinkle_main,
+    product_box_image: wrinkle_box,
+    phone_number: "+989125180421",
+    email: "info@lanocare.com",
+  },
+  {
+    id: 2,
+    name: "Skin Care",
+    description:
+      "Skin care is very important. LANOCARE body lotion, while providing powerful hydration together with fast absorption and providing a soft and light texture and deep penetration into the skin, increases transparency and freshness of the skin and prevents dryness. Besides regenerating and revitalizing the skin, this product also helps relieve itching and skin inflammation. It also has an attractive and long-lasting scent and does not contain parabens. LANOCARE body lotion soothes sensitive and inflamed skin and minimizes the feeling of stretching caused by dry skin. This lotion has many benefits with its unique",
+    short_description:
+      "ingredients including the following: Sodium Hyaluronate The hydration effect is the most important role of sodium hyaluronate. Sodium hyaluronate has an extraordinary water absorption capacity that can absorb 1000 times its own volume of water. It also prevents wrinkles and skin aging due to skin nutrition. Shea Butter Shea butter is a safe and powerful moisturizer and softener for skin tissue and has antioxidant properties. Antioxidants play an important role in reducing and controlling the activity of free radicals and oxidant reactions, they postpone the appearance of aging signs and prevent skin wrinkles.Usable For: Body           Hands         Neck",
+    price: 220,
+    feature_image: skin_featured,
+    product_image: skin_main,
+    product_box_image: skin_box,
+    phone_number: "+989125180421",
+    email: "info@lanocare.com",
+  },
+  {
+    id: 3,
+    name: "Body Lotion",
+    description:
+      "LANOCARE body lotion is suitable for sensitive skin, does not contain fragrance or essence, and its compounds eliminate the effect of detergents such as soap on the skin. In addition to regenerating and revitalizing the skin, this product also helps relieve itching and inflammation of the skin, and due to the absence of parabens, it is ideal for sensitive skin and minimizes the feeling of stretching caused by dry skin. LANOCARE lotion has many benefits due to its unique ingredients.",
+    short_description:
+      "ingredients of this product are as following: Allantoin Allantoin has anti-aging properties and maintains skin moisture. Another benefit of allantoin for the skin is helping relieve eczema through moisturizing and relieving skin itching. This substance exfoliates dead and damaged cells and improves skin texture. As a result, the skin looks so clear. Sodium Hyaluronate: Hydrating is the most important role of sodium hyaluronate. It also prevents wrinkles and skin aging due to skin nutrition. Usable For: Body           Hands         Neck",
+    price: 185,
+    feature_image: body_featured,
+    product_image: body_main,
+    product_box_image: body_box,
+    phone_number: "+989125180421",
+    email: "info@lanocare.com",
+  },
+  {
+    id: 4,
+    name: "Vitam C",
+    description:
+      "This serum containing vitamin C compensates for the lack of moisture in the skin and improves the dryness of the skin. Vitamin C is a strong antioxidant that neutralizes free radicals and softens wrinkles, brightens the skin, and with regular use twice a day, fades pigmentation and minimizes blemishes. It accelerates collagen and increases the elasticity of the epidermal layer. It also nourishes the skin and reduces the speed and signs of skin aging.",
+    short_description:
+      "LANOCARE vitamin C serum contains alpha-arbutin 2.5 and ascorbic acid 10%. Ascorbic Acid Ascorbic acid increases your skin's collagen production so that you have firmer and stronger skin whose tiny lines and wrinkles have faded away. This substance has strong antioxidant properties and helps fight free radicals and damage caused by UV rays. It also contains Alpha Arbutin. Alpha Arbutin Having a well-known and very efficient composition, it is the best alternative to hydroquinone. In addition to its very good brightening power, it contains antioxidants that help rejuvenate the skin. Usable For: Facial and Neck Skin               Hands Suitable for all types of skin, except for very oily and acne-prone skin",
+    price: 120,
+    feature_image: vitaminc_featured,
+    product_image: vitmainc_main,
+    product_box_image: vitaminc_box,
+    phone_number: "+989125180421",
+    email: "info@lanocare.com",
+  },
+  {
+    id: 5,
+    name: "Vitamin E",
+    description:
+      "Fluid sunscreens are water-based, that's why they are light and quick to absorb, and generally have high hydration and help maintain the moisture in the skin. Another benefit of this type of sunscreen is reducing the amount of skin pore blockage which prevents acne. LANOCARE Fluid sunscreen is very light with fast and effective absorption and is suitable for all skin types, especially oily skin. This sunscreen has maximum protection against UVA-UVB-IR rays and also contains moisturizing and anti-inflammatory compounds.",
+    short_description:
+      "Among the compounds used in this product, which are very effective in skin health, the followings can be mentioned. Sodium Hyaluronate Very strong hydration and water retention of the skin is one of the properties of this material. Almond Oil: Hydrating, moisturizing and excellent sun protection are the properties of this oil. Salicylic Acid It is an anti-inflammatory, surface exfoliator and skin oil control agent. Usable for: Facial and Neck Skin              Hands Suitable for all different types of skin.",
+    price: 190,
+    feature_image: vitamine_featured,
+    product_image: vitamine_main,
+    product_box_image: vitamine_box,
+    phone_number: "+989125180421",
+    email: "info@lanocare.com",
+  },
+];
