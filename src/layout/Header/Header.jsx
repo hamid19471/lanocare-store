35b2842ffb429f2d61ff67f2e8d61e4db9aefe40
hamid19471 <hamid.19471@gmail.com>
2023-05-24 23:33:40 +0330

    Update: add some HTML and style for header

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,4 +1,11 @@
 const Header = () => {
-  return <div>this is Header</div>;
+  return (
+    <header className="container">
+      <div className="bg-slate-100 w-full rounded-full opacity-80 flex items-center justify-between py-6 px-5 mt-8">
+        <div>Menu items</div>
+        <div>Logo</div>
+      </div>
+    </header>
+  );
 };
 export default Header;
