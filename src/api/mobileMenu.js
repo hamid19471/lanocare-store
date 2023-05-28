a9e1150aa30c8eabc0af0e4369feeef8e5587a94
hamid19471 <hamid.19471@gmail.com>
2023-05-28 13:14:01 +0330

    Create: Mobile menu items for responsive design (Fake Data)

---
+++ src/api/mobileMenu.js
@@ -0,0 +1,22 @@
+export const mobileMenu = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "Shop",
+    path: "/shop",
+  },
+  {
+    id: 3,
+    name: "Cart",
+    path: "/cart",
+  },
+  {
+    id: 4,
+    name: "Dashboard",
+    path: "/dashboard",
+  },
+];
