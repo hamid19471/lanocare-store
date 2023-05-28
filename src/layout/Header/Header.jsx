b6c85f8efc393a3c87d904e55f9dc75e09c45682
hamid19471 <hamid.19471@gmail.com>
2023-05-28 13:14:59 +0330

    Update: fix some issues and add more styles

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,10 +1,11 @@
 import { menuItems } from "../../api/menuItems";
+import { mobileMenu } from "../../api/mobileMenu";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/Main-Logo.png";
 const Header = () => {
   return (
     <header className="container">
-      <div className="bg-slate-100 w-full rounded-full opacity-80 flex items-center justify-between py-3 px-10 mt-8">
+      <div className="hidden bg-slate-100 w-full rounded-full opacity-80 md:flex items-center justify-between py-3 px-10 mt-8">
         <div>
           <ul>
             {menuItems.map((item) => (
@@ -22,6 +23,18 @@ const Header = () => {
             <img src={logo} alt="Lanocare" className="w-24" />
           </Link>
         </div>
+      </div>
+      <div
+        className="md:hidden text-white fixed bottom-0 flex items-center justify-around gap-8 left-1/2 -translate-y-1/2 -translate-x-1/2
+            w-[470px] border-2 border-primary rounded-full py-5 px-8 bg-primary/60 backdrop:blur-lg"
+      >
+        <ul className="flex justify-center items-center gap-14">
+          {mobileMenu.map((item) => (
+            <li key={item.id}>
+              <NavLink to={item.path}>{item.name}</NavLink>
+            </li>
+          ))}
+        </ul>
       </div>
     </header>
   );
