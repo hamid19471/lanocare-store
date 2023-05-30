08759d3842f4d7f835aeca60cc954f5f96e8056c
hamid19471 <hamid.19471@gmail.com>
2023-05-30 23:10:09 +0330

    Update: add onclick and pass as props for use dynamicly

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,8 @@
-const Button = ({ primary, children }) => {
+const Button = ({ primary, children, onClick }) => {
   return (
     <div>
       <button
+        onClick={onClick}
         className={`py-3 px-5 rounded-xl hover:scale-110 duration-300 ${
           primary
             ? "bg-primary"
