4c4b0ef97d64362c005303886eb43efd93d80c10
hamid19471 <hamid.19471@gmail.com>
2023-06-01 00:39:49 +0330

    Update: use main layout for all page and other routes (refactor)

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,30 +4,37 @@ import About from "./pages/About/About";
 import Blog from "./pages/Blog/Blog";
 import Contact from "./pages/Contact/Contact";
 import Shop from "./pages/Shop/Shop";
+import Layout from "./layout";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/shop",
-    element: <Shop />,
-  },
-  {
-    path: "/blog",
-    element: <Blog />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-  {
-    path: "*",
-    element: <h1>Not Found</h1>,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/shop",
+        element: <Shop />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "*",
+        element: <h1>Not Found</h1>,
+      },
+    ],
   },
 ]);
 
