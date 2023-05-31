2cc63395702c5b1b1a1d7a5cb2f4df63c174adf4
hamid19471 <hamid.19471@gmail.com>
2023-06-01 01:06:25 +0330

    Update: add login/signup and search icon in navbar and set logo in center

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -2,11 +2,13 @@ import { menuItems } from "../../api/menuItems";
 import { mobileMenu } from "../../api/mobileMenu";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/Main-Logo.png";
+import { CiUser } from "react-icons/ci";
+import { CiSearch } from "react-icons/ci";
 const Header = () => {
   return (
     <header className="container relative z-10">
-      <div className="hidden bg-slate-100 backdrop-blur-3xl w-full rounded-full opacity-80 md:flex items-center justify-between py-3 px-10 mt-8">
-        <div>
+      <div className="hidden bg-slate-100 backdrop-blur-3xl w-full rounded-full opacity-80 lg:flex items-center justify-between py-3 px-10 mt-8">
+        <div className="w-full">
           <ul>
             {menuItems.map((item) => (
               <li
@@ -18,14 +20,24 @@ const Header = () => {
             ))}
           </ul>
         </div>
-        <div>
+        <div className="w-full">
           <Link to="/">
             <img src={logo} alt="Lanocare" className="w-24" />
           </Link>
         </div>
+        <div>
+          <ul className="flex items-center justify-center gap-3 ">
+            <li>
+              <CiUser size={23} />
+            </li>
+            <li>
+              <CiSearch size={23} />
+            </li>
+          </ul>
+        </div>
       </div>
       <div
-        className="md:hidden text-white fixed bottom-0 flex items-center justify-around gap-8 left-1/2 -translate-y-1/2 -translate-x-1/2
+        className="lg:hidden text-white fixed bottom-0 flex items-center justify-around gap-8 left-1/2 -translate-y-1/2 -translate-x-1/2
             w-[470px] border-2 border-primary rounded-full py-5 px-8 bg-primary/60 backdrop:blur-lg"
       >
         <ul className="flex justify-center items-center gap-14">
