ad1d9cb8409939afdc6b0f401c991c987e641a23
hamid19471 <hamid.19471@gmail.com>
2023-05-31 23:29:24 +0330

    Creat: routes for route map in application

---
+++ src/routes.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Blog from "./pages/Blog/Blog";
+import Contact from "./pages/Contact/Contact";
+import Shop from "./pages/Shop/Shop";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/shop",
+    element: <Shop />,
+  },
+  {
+    path: "/blog",
+    element: <Blog />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "*",
+    element: <h1>Not Found</h1>,
+  },
+]);
+
+export default router;
