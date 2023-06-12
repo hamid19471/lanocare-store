0ea6df67d654ff24462328b506bccc927ebaf6f9
hamid19471 <hamid.19471@gmail.com>
2023-06-13 00:47:52 +0330

    Update: warp content by context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import CartProvider from "./context/Cart/CartProvider";
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <CartProvider>
+      <RouterProvider router={router} />;
+      <ToastContainer />
+    </CartProvider>
+  );
 }
 
 export default App;
