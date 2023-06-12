2301279cf1ccb2950f6a224cbf89dd4ba5dd6de6
hamid19471 <hamid.19471@gmail.com>
2023-06-13 00:49:36 +0330

    Update: add toasify for appearance when add to cart button clicked

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,5 +1,19 @@
+import { toast } from "react-toastify";
 import Button from "../../components/Button/Button";
+import { useCartActions } from "../../context/Cart/CartProvider";
+
 const ProductCard = ({ item }) => {
+  const dispatch = useCartActions();
+  const handleAddToCart = (item) => {
+    dispatch({ type: "ADD_TO_CART", payload: item });
+    toast(`${item.name} added to cart`, {
+      position: "bottom-center",
+      className: "bg-primary text-black font-semibold",
+      autoClose: 2000,
+      theme: "dart",
+      progressClassName: "bg-black/80",
+    });
+  };
   return (
     <div className=" flex flex-col gap-12 bg-black/30 p-4 rounded-lg m-3">
       <div className="overflow-hidden">
@@ -15,7 +29,9 @@ const ProductCard = ({ item }) => {
       </div>
       <div className="flex gap-3 mb-6">
         <Button>Read More</Button>
-        <Button primary="primary">Add To Cart</Button>
+        <Button primary="primary" onClick={() => handleAddToCart(item)}>
+          Add To Cart
+        </Button>
       </div>
     </div>
   );
