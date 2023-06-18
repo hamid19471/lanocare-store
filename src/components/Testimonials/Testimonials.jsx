82776718b92aa00775971e17feb8928027bc32cc
hamid19471 <hamid.19471@gmail.com>
2023-06-19 00:05:03 +0330

    Create: create and design testimonials component

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,32 @@
+import Slider from "react-slick";
+import TestimonialsCard from "../TestimonialCard/TestimonialCard";
+import { TestimonialsData } from "../../api/TestimonialsData";
+
+const Testimonials = () => {
+  const settings = {
+    Infinite: true,
+    LazyLoad: true,
+    speed: 500,
+    slidesToShow: 2,
+    ceneterMode: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    arrows: false,
+    dots: true,
+    dotsClass: "slick-dots slick-thumb",
+  };
+
+  return (
+    <div className="container mb-28">
+      <Slider {...settings}>
+        {TestimonialsData.map((item) => (
+          <div key={item.id} className="">
+            <TestimonialsCard item={item} />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+export default Testimonials;
