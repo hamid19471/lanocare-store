76b71330403fbf7d0fd5076caefd79992427d354
hamid19471 <hamid.19471@gmail.com>
2023-06-19 00:03:52 +0330

    Create: sample data for testimonials

---
+++ src/api/TestimonialsData.js
@@ -0,0 +1,40 @@
+import avatar from "../assets/Testimonials-avatar.png";
+import icons from "../assets/Testimonials-icon.png";
+export const TestimonialsData = [
+  {
+    id: 1,
+    image: avatar,
+    icons: icons,
+    name: "John Doe",
+    title: "CEO",
+    description:
+      "I have just started using Lanoker's skincare and haircare products, so I haven't seen any noticeable results yet. However, I must say that besides the attractive packaging, the products seem to be of high quality and made with natural ingredients. Therefore, I hope that with continuous use of these products, I will see a significant improvement in the condition of my skin and hair.",
+  },
+  {
+    id: 2,
+    image: avatar,
+    icons: icons,
+    name: "Jane Doe",
+    title: "CEO",
+    description:
+      "I have just started using Lanoker's skincare and haircare products, so I haven't seen any noticeable results yet. However, I must say that besides the attractive packaging, the products seem to be of high quality and made with natural ingredients. Therefore, I hope that with continuous use of these products, I will see a significant improvement in the condition of my skin and hair.",
+  },
+  {
+    id: 3,
+    image: avatar,
+    icons: icons,
+    name: "John Smith",
+    title: "CEO",
+    description:
+      "I am really satisfied with Lanoker's skincare and haircare products. Using their products has made my skin soft and smooth, and my hair remains healthy and shiny without any damage. Lanoker products are made with natural ingredients and have a pleasant scent. Additionally, they have beautiful and attractive packaging, which makes purchasing and using them enjoyable.",
+  },
+  {
+    id: 4,
+    image: avatar,
+    icons: icons,
+    name: "Jane Smith",
+    title: "CEO",
+    description:
+      "I have been workin with Lanoker for a while now, and I must say that I am very impressed with their products. I have been using their skincare and haircare products for a while now, and I have seen a significant improvement in the condition of my skin and hair. I would highly recommend Lanoker's products to anyone who wants to improve the condition of their skin and hair.",
+  },
+];
