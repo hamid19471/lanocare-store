f25a7a9f16adcca0ad487fa3bca062fdf0e824f5
hamid19471 <hamid.19471@gmail.com>
2023-06-23 12:57:11 +0330

    Update: blog section

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,4 +1,14 @@
+import { BlogData } from "../../api/BlogData";
+import BlogCard from "../../components/BlogCard/BlogCard";
 const Blog = () => {
-  return <div className="container">this is Blog</div>;
+  return (
+    <div className="container flex gap-3">
+      {BlogData.map((item) => (
+        <div key={item.id}>
+          <BlogCard item={item} />
+        </div>
+      ))}
+    </div>
+  );
 };
 export default Blog;
