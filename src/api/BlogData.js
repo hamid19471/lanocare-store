f42581193b66811bace3f957b6abe98552de4d59
hamid19471 <hamid.19471@gmail.com>
2023-06-23 11:52:18 +0330

    Create: blog dummy data

---
+++ src/api/BlogData.js
@@ -0,0 +1,42 @@
+export const BlogData = [
+  {
+    id: 1,
+    title: "The Cosmetics World",
+    description:
+      "Est diam debitis an, error recusabo id pro, quo eripuit civibus ut. Mel ut tamquam erroribus, ad nonumy vituperata mei.Et qui falli latine consequuntur. In appellantur concludaturque pro. Commune scriptorem",
+    date: "2021-01-01",
+    image: "",
+    author: "Admin",
+    category: "Cosmetics",
+  },
+  {
+    id: 2,
+    title: "Prep a good primer",
+    description:
+      "Est diam debitis an, error recusabo id pro, quo eripuit civibus ut. Mel ut tamquam erroribus, ad nonumy vituperata mei.Et qui falli latine consequuntur. In appellantur concludaturque pro. Commune scriptorem",
+    date: "2021-01-02",
+    image: "",
+    author: "Admin",
+    category: "Cosmetics",
+  },
+  {
+    id: 3,
+    title: "Know how to apply it",
+    description:
+      "Est diam debitis an, error recusabo id pro, quo eripuit civibus ut. Mel ut tamquam erroribus, ad nonumy vituperata mei.Et qui falli latine consequuntur. In appellantur concludaturque pro. Commune scriptorem",
+    date: "2021-01-03",
+    image: "",
+    author: "Admin",
+    category: "Cosmetics",
+  },
+  {
+    id: 4,
+    title: "50 Shades of Great",
+    description:
+      "Est diam debitis an, error recusabo id pro, quo eripuit civibus ut. Mel ut tamquam erroribus, ad nonumy vituperata mei.Et qui falli latine consequuntur. In appellantur concludaturque pro. Commune scriptorem",
+    date: "2021-01-04",
+    image: "",
+    author: "Admin",
+    category: "Cosmetics",
+  },
+];
