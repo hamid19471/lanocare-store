fc0b7bc7b98968176b7b7a0c3e9e2a8a28b25f69
hamid19471 <hamid.19471@gmail.com>
2023-07-08 10:40:09 +0330

    Update: remove routes from file

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,8 +6,6 @@ import Contact from "./pages/Contact/Contact";
 import Shop from "./pages/Shop/Shop";
 import Layout from "./layout";
 import Privacy from "./pages/Privacy/Privacy";
-import Signup from "./pages/Signup/Signup";
-import Signin from "./pages/Signin/Signin";
 import Notfound from "./pages/404";
 
 const router = createBrowserRouter([
@@ -44,14 +42,6 @@ const router = createBrowserRouter([
   {
     path: "*",
     element: <Notfound />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/signin",
-    element: <Signin />,
   },
 ]);
 
